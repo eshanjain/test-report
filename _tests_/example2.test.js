b380234aec7634f6690661907c064fb9154b5b88
eshanjain <eshanjain1000@gmail.com>
2024-08-29 10:52:06 -0700

    Create example2.test.js

---
+++ _tests_/example2.test.js
@@ -0,0 +1,41 @@
+describe('Test Suite 1', () => {
+  test('Test 1', () => {
+    expect(true).toBe(true);
+  });
+
+  test('Test 2', () => {
+    expect(true).toBe(true);
+  });
+
+  test('Test 3', () => {
+    expect(true).toBe(true);
+  });
+
+  test('Test 4', () => {
+    expect(true).toBe(true);
+  });
+
+  test('Test 5', () => {
+    expect(true).toBe(true);
+  });
+
+  test('Test 6', () => {
+    expect(true).toBe(true);
+  });
+
+  test('Test 7', () => {
+    expect(true).toBe(true);
+  });
+
+  test('Test 8', () => {
+    expect(true).toBe(true);
+  });
+
+  test('Test 9', () => {
+    expect(true).toBe(true);
+  });
+
+  test('Test 10', () => {
+    expect(true).toBe(true);
+  });
+});
