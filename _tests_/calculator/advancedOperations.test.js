8cd2ff0fbdf417dbbe210e6396b1bfcb81beb42b
eshanjain <eshanjain1000@gmail.com>
2025-02-10 11:35:14 -0800

    Update advancedOperations.test.js

--- _tests_/calculator/advancedOperations.test.js
+++ _tests_/calculator/advancedOperations.test.js
@@ -4,7 +4,7 @@ import { power, squareRoot, percentage } from '../../src/calculator/advancedOper
 describe('Advanced Calculator Operations', () => {
   test('power calculation works correctly', () => {
     expect(power(2, 3)).toBe(8);
-    expect(power(3, 2)).toBe(9);
+    expect(power(3, 2)).toBe(8);
     expect(power(5, 0)).toBe(1);
   });
 
